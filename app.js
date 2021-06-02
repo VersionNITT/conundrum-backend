378a223a17ad9bd6d99f40b7793723f6abfc6f2b
harshrai654 <harshraitth2@gmail.com>
2021-06-03 00:16:44 +0530

    Fixed flash in login, changed default port

--- app.js
+++ app.js
@@ -1,36 +1,33 @@
 require("dotenv").config();
-const express = require('express');
-const mongoose = require('mongoose');
-const passport = require('passport');
-const session = require('express-session');
+const express = require("express");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const session = require("express-session");
 
 const app = express();
 
 // Passport Config
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
 // DB Config
 const db = process.env.MONGOURI;
 
 // Connect to MongoDB
 mongoose
-  .connect(
-    db,
-    { useNewUrlParser: true ,useUnifiedTopology: true}
-  )
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
+	.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => console.log("MongoDB Connected"))
+	.catch((err) => console.log(err));
 
 // Express body parser
 app.use(express.urlencoded({ extended: true }));
 
 // Express session
 app.use(
-  session({
-    secret: process.env.SECRET,
-    resave: true,
-    saveUninitialized: true
-  })
+	session({
+		secret: process.env.SECRET,
+		resave: true,
+		saveUninitialized: true,
+	})
 );
 
 // Passport middleware
@@ -38,9 +35,9 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Routes
-app.use('/', require('./routes/index.js'));
-app.use('/users', require('./routes/users.js'));
+app.use("/", require("./routes/index.js"));
+app.use("/users", require("./routes/users.js"));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4080;
 
 app.listen(PORT, console.log(`Server running on  ${PORT}`));
